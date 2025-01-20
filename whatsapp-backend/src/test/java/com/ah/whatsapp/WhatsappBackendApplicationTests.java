1e409a39ef809a9ffac7263b7f7c66f33f22ede1
AhmedHaris <haris.mirza@hq.bill.com>
2025-01-21 02:22:19 +0500

    initialize WhatsApp backend project structure with Gradle and Spring Boot

---
+++ whatsapp-backend/src/test/java/com/ah/whatsapp/WhatsappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ah.whatsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
